ab801bf019b06fcea084b07724d730f0dd0dcb88
Timur Lagawy <timurtagirovich@mail.ru>
2023-07-02 19:34:16 +0200

    seventh_day_loops

---
+++ src/main/java/com/epam/learn/main/TestDoWhile.java
@@ -0,0 +1,21 @@
+package com.epam.learn.main;
+
+public class TestDoWhile {
+    public static void main(String[] args) {
+//        int i=0;
+//        do {
+//            System.out.println("Iteration " + i);}
+//        while (i>5 && 10<i); //do at once
+//    }
+//        int i=0;
+//        while (true) {
+//            if (i>8){
+//                break;}
+//            System.out.println(i++);
+//        }
+        int i = 0;
+        while (i++ <10) {
+            if( i==4 || i==8){continue;
+        }
+            System.out.println(i);
+}}}
--- src/main/java/com/epam/learn/main/TestWhile.java
+++ src/main/java/com/epam/learn/main/TestWhile.java
@@ -2,9 +2,27 @@ package com.epam.learn.main;
 
 public class TestWhile {
     public static void main(String[] args) {
-        int i=0;
+       /* int i=0;
     while (i<5) {
         System.out.println("Iteration " + i);
         i++;
-    }}
-}
+    }}*/
+        /*int i = 0;
+       outer: while ( i < 5){
+           int j = 0;
+           while ( j < 2){
+               j++;
+               System.out.println("i = " + i +" j = " + j);
+              break outer ;
+           }*/
+        int i = 0;
+        outer: while (i++ < 3){
+            int j = 0;
+            while (j++ < 5){
+                if (j == 2){
+                    continue outer;
+                }
+                System.out.println("i = " + i + " j = " + j);
+            }
+        }
+}}
