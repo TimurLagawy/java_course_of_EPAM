be9e3f2184644cfdd401e480ca5d9a1be366bd5e
Timur Lagawy <timurtagirovich@mail.ru>
2023-07-03 22:37:38 +0200

    eighth_day_task_snail_string2

--- src/main/java/com/epam/learn/main/Snail.java
+++ src/main/java/com/epam/learn/main/Snail.java
@@ -4,30 +4,50 @@ import java.util.Scanner;
 
 public class Snail {
     public static void main(String[] args) {
-        Scanner scannerA = new Scanner (System.in);
-        String a = scannerA.nextLine();
-        Scanner scannerB = new Scanner (System.in);
-        String b = scannerB.nextLine();
-        Scanner scannerTop = new Scanner (System.in);
-        String top = scannerTop.nextLine();
+       /* Scanner scanner = new Scanner (System.in);
+        String nums = scanner.nextLine();
+        System.out.println(nums);
+        String [] parts = nums.split(System.lineSeparator());
 
-        int a1  = Integer.parseInt(a);
-        int b1 = Integer.parseInt(b);
-        int top1 = Integer.parseInt(top);
+        int a1  = Integer.parseInt(parts[0]);
+        int b1 = Integer.parseInt(parts[1]);
+        int top1 = Integer.parseInt(parts[2]);
+
+        System.out.println();
 
         for (int i = 1; i <= 15 ; i++) {
             int h1 = a1 * i;
             h1 = (h1 - (b1 * (i - 1)));
             if (h1 >= top1) {
-                String nums = ("" + i + ", " + a + ", " + b + ", " + top);
-                System.out.println(nums);
+                String num = ("" + i + ", " + a1 + ", " + b1 + ", " + top1);
+                System.out.println(num);
                 break;
             }
             if( i >14){
-                String impos = ("Impossible" + ", " + a + ", " + b + ", " + top);
+                String impos = ("Impossible" + ", " + a1 + ", " + b1 + ", " + top1);
                 System.out.println(impos);
                 break;
             }
         }
+    }*/
+
+            Scanner scanner = new Scanner (System.in);
+            int a, b, h;
+            a = scanner.nextInt();
+            b = scanner.nextInt();
+            h = scanner.nextInt();
+
+            for (int i = 1; i <= 15 ; i++) {
+                int h1 = a * i;
+                h1 = (h1 - (b * (i - 1)));
+                if (h1 >= h) {
+                    System.out.println(i);
+                    break;
+                }
+                if( i >14){System.out.println("Impossible");
+                    break;
+                }
+            }
+        }
     }
-}
+
