b1833e7365033fdf8b3327fc138ced02b00064df
Timur Lagawy <timurtagirovich@mail.ru>
2023-07-03 21:28:00 +0200

    eighth_day_task_snail_string

--- src/main/java/com/epam/learn/main/Snail.java
+++ src/main/java/com/epam/learn/main/Snail.java
@@ -5,21 +5,29 @@ import java.util.Scanner;
 public class Snail {
     public static void main(String[] args) {
         Scanner scannerA = new Scanner (System.in);
-        int a = scannerA.nextInt();
+        String a = scannerA.nextLine();
         Scanner scannerB = new Scanner (System.in);
-        int b = scannerB.nextInt();
-        Scanner scannerH = new Scanner (System.in);
-        int h = scannerH.nextInt();
+        String b = scannerB.nextLine();
+        Scanner scannerTop = new Scanner (System.in);
+        String top = scannerTop.nextLine();
+
+        int a1  = Integer.parseInt(a);
+        int b1 = Integer.parseInt(b);
+        int top1 = Integer.parseInt(top);
 
         for (int i = 1; i <= 15 ; i++) {
-            int h1 = a * i;
-            h1 = (h1 - (b * (i - 1)));
-            if (h1 >= h) {
-                System.out.println(i);
+            int h1 = a1 * i;
+            h1 = (h1 - (b1 * (i - 1)));
+            if (h1 >= top1) {
+                String nums = ("" + i + ", " + a + ", " + b + ", " + top);
+                System.out.println(nums);
                 break;
             }
-            if( i >14){System.out.println("Impossible");
-                break;}
+            if( i >14){
+                String impos = ("Impossible" + ", " + a + ", " + b + ", " + top);
+                System.out.println(impos);
+                break;
             }
         }
+    }
 }
