c5d77f66d121638b0740df57a6cccc390c2dd7d7
Timur Lagawy <timurtagirovich@mail.ru>
2023-07-03 17:18:29 +0200

    eighth_day_task_snail

---
+++ src/main/java/com/epam/learn/main/Snail.java
@@ -0,0 +1,25 @@
+package com.epam.learn.main;
+
+import java.util.Scanner;
+
+public class Snail {
+    public static void main(String[] args) {
+        Scanner scannerA = new Scanner (System.in);
+        int a = scannerA.nextInt();
+        Scanner scannerB = new Scanner (System.in);
+        int b = scannerB.nextInt();
+        Scanner scannerH = new Scanner (System.in);
+        int h = scannerH.nextInt();
+
+        for (int i = 0; i <= 15 ; i++) {
+            int h1 = a * i;
+            h1 = (h1 - (b * (i - 1)));
+            if (h1 >= h) {
+                System.out.println(i);
+                break;
+            }
+            if( i >14){System.out.println("Impossible");
+                break;}
+            }
+        }
+}
